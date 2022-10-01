a99986d1075534d8c64aceaa9637c1cff3a5ef46
Moha Dmn <91742203+mohadmn03@users.noreply.github.com>
2022-10-01 14:00:24 +0100

    Update PropertyList.jsx

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -5,7 +5,7 @@ const PropertyList = () => {
   return (
     <div className="pList">
         <div className="pListItem">
-            <img src="https://scontent.faae2-2.fna.fbcdn.net/v/t1.6435-9/169679857_4506993532662856_3649545272680751141_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHvnUsutrwx-0nMZMr-tz91OA_E_ODCWFM4D8T84MJYU2bUtltpIBV1FFqDtxo3cpcm6rr8kFBjKI00TimrOGYh&_nc_ohc=vAp8IrQ1XlAAX8G36Tk&tn=3SO9fpAfYQS3KJgY&_nc_ht=scontent.faae2-2.fna&oh=00_AT_Oj_-7H3dd9RARrb4o1aiIVcF6mcd_t3kPuoWrT-wyMw&oe=62E0CF9C"
+            <img src="https://q-xx.bstatic.com/psb/capla/static/media/long_stays_banner.0c811e7a.png"
             alt=""
             className="pListImg" />
             <div className="pListTitles">
@@ -14,7 +14,7 @@ const PropertyList = () => {
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://scontent.faae2-2.fna.fbcdn.net/v/t1.6435-9/169679857_4506993532662856_3649545272680751141_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHvnUsutrwx-0nMZMr-tz91OA_E_ODCWFM4D8T84MJYU2bUtltpIBV1FFqDtxo3cpcm6rr8kFBjKI00TimrOGYh&_nc_ohc=vAp8IrQ1XlAAX8G36Tk&tn=3SO9fpAfYQS3KJgY&_nc_ht=scontent.faae2-2.fna&oh=00_AT_Oj_-7H3dd9RARrb4o1aiIVcF6mcd_t3kPuoWrT-wyMw&oe=62E0CF9C"
+            <img src="https://q-xx.bstatic.com/psb/capla/static/media/long_stays_banner.0c811e7a.png"
             alt=""
             className="pListImg" />
             <div className="pListTitles">
@@ -23,7 +23,7 @@ const PropertyList = () => {
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://scontent.faae2-2.fna.fbcdn.net/v/t1.6435-9/169679857_4506993532662856_3649545272680751141_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHvnUsutrwx-0nMZMr-tz91OA_E_ODCWFM4D8T84MJYU2bUtltpIBV1FFqDtxo3cpcm6rr8kFBjKI00TimrOGYh&_nc_ohc=vAp8IrQ1XlAAX8G36Tk&tn=3SO9fpAfYQS3KJgY&_nc_ht=scontent.faae2-2.fna&oh=00_AT_Oj_-7H3dd9RARrb4o1aiIVcF6mcd_t3kPuoWrT-wyMw&oe=62E0CF9C"
+            <img src="https://q-xx.bstatic.com/psb/capla/static/media/long_stays_banner.0c811e7a.png"
             alt=""
             className="pListImg" />
             <div className="pListTitles">
@@ -32,7 +32,7 @@ const PropertyList = () => {
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://scontent.faae2-2.fna.fbcdn.net/v/t1.6435-9/169679857_4506993532662856_3649545272680751141_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHvnUsutrwx-0nMZMr-tz91OA_E_ODCWFM4D8T84MJYU2bUtltpIBV1FFqDtxo3cpcm6rr8kFBjKI00TimrOGYh&_nc_ohc=vAp8IrQ1XlAAX8G36Tk&tn=3SO9fpAfYQS3KJgY&_nc_ht=scontent.faae2-2.fna&oh=00_AT_Oj_-7H3dd9RARrb4o1aiIVcF6mcd_t3kPuoWrT-wyMw&oe=62E0CF9C"
+            <img src="https://q-xx.bstatic.com/psb/capla/static/media/long_stays_banner.0c811e7a.png"
             alt=""
             className="pListImg" />
             <div className="pListTitles">
@@ -41,7 +41,7 @@ const PropertyList = () => {
             </div>
         </div>
         <div className="pListItem">
-            <img src="https://scontent.faae2-2.fna.fbcdn.net/v/t1.6435-9/169679857_4506993532662856_3649545272680751141_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHvnUsutrwx-0nMZMr-tz91OA_E_ODCWFM4D8T84MJYU2bUtltpIBV1FFqDtxo3cpcm6rr8kFBjKI00TimrOGYh&_nc_ohc=vAp8IrQ1XlAAX8G36Tk&tn=3SO9fpAfYQS3KJgY&_nc_ht=scontent.faae2-2.fna&oh=00_AT_Oj_-7H3dd9RARrb4o1aiIVcF6mcd_t3kPuoWrT-wyMw&oe=62E0CF9C"
+            <img src="https://q-xx.bstatic.com/psb/capla/static/media/long_stays_banner.0c811e7a.png"
             alt=""
             className="pListImg" />
             <div className="pListTitles">
