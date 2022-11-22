bd65599f3c2bef48925d1ddbee2068c88c8ca11e
Moha Dmn <91742203+mohadmn03@users.noreply.github.com>
2022-11-22 15:46:07 +0400

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         <Featured/>
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
-        <h1 className="homeTitle">TESTTTTTTTTTTTTTT</h1>
+        <h1 className="homeTitle">Home guests love</h1>
         <FeaturedProperties />
         <MailList />
         <Footer/>
